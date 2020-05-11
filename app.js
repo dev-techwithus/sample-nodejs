5e06b56a3f2c75bf78c7f489956fe7c981223f1a
Sashwat K <sashwat0001@gmail.com>
2020-05-11 22:49:44 +0530

    Added new structure

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!, from TechWithUs'))
+const parameters = require('./parameters');
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', (req, res) => {
+    parameters.helloworldJSON.statement = 'Hello world';
+    parameters.helloworldJSON.author = 'Tech With Us PVT LTD';
+
+    res.send(parameters.helloworldJSON);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
---
+++ parameters.js
@@ -0,0 +1,1 @@
+module.exports.helloworldJSON = JSON.parse('{"statement" : "","author": ""}');
